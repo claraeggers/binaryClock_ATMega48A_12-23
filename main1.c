47f69048aa9b929e4e96d9f24452318520f0b6b4
clara.eggers <clara.eggers@stud.htwk-leipzig.de>
2024-03-18 09:55:45 +0100

    first try sleep function

--- main1.c
+++ main1.c
@@ -6,8 +6,7 @@
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <util/delay.h>
+
 
 volatile uint8_t sekunde = 0;
 volatile uint8_t minute = 0;
@@ -17,8 +16,11 @@ volatile uint8_t hourBitShiftUp;
 volatile uint8_t prell = 0;
 volatile uint8_t pwm = 0;
 volatile bool sleep_mode_on = false;
+volatile uint8_t prellS = 0;
+volatile uint8_t prellM = 0;
+volatile uint8_t prellH = 0;
 
-void pwm_fkt(volatile uint8_t pwm, volatile bool sleep_mod_on){
+void pwm_fkt(volatile uint8_t pwm, volatile bool sleep_mode_on){
 
     if(sleep_mode_on == false){
 
@@ -45,9 +47,18 @@ void pwm_fkt(volatile uint8_t pwm, volatile bool sleep_mod_on){
     }
 }
 
+void sleep(volatile bool sleep_mode_on){
+
+    while(sleep_mode_on == true){
+    
+    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+    sleep_mode();
+    }
+}
+
 
 int main (void){
-    //minute = 0;
+ 
 
     //EXT CLOCK SOURCE
     ASSR |= (1 << AS2);
@@ -55,34 +66,44 @@ int main (void){
     //TIMER2 OVF CTC
     TIMSK2 |= (1<<TOIE2) ; //enable compare interrupt, //enable overflow 
     TCCR2B |= (1 << CS20) | (1 << CS22);     //ps=128, Timer 2, (32,768 kHz = 32768/128*256 = 1 1/s == 1s)
+
     //WATCHDOG
-   // MCUSR &= ~(1 << WDRF); // Watchdog-Reset löschen
-    //wdt_disable();
-    //WDTCSR |= (1 << WDIE) | (1 << WDE) | (1 << WDP3) | (1 << WDP0); // WDT auf 20 Sekunden einstellen
-    //wdt_enable(WDTO_8S);
+    MCUSR &= ~(1 << WDRF); // Watchdog-Reset löschen
+    wdt_disable();
+    WDTCSR |= (1 << WDIE) | (1 << WDE) | (1 << WDP3) | (1 << WDP0); // WDT auf 20 Sekunden einstellen
+    wdt_enable(WDTO_8S);
+
+    //TIMER0 CTC for PWM
     TCCR0B |= (1<<CS01);
     OCR0A = 255;
     TCCR0A |= (1<< WGM00);
     TIMSK0 |= (1<<OCIE0A);
+
+    //SET Interrupts
+    EIMSK |= (1<<INT0) | (1<<INT1); // enable interupt0 and interrupt 1
+    EICRA |= (1<<ISC11) | (1<<ISC01); // enable external interrupt 0/1 auf fallende flanke
+    PCICR |= (1<<PCIE0); // pin change interrupt enable for hourcounter in pcint 0-7
+
+    //SLEEPMODE
+
   
     //GLOBAL
     sei();  
 
-   DDRC = 0b0111111;
-   DDRD = 0b11100000;
-   DDRB = 0b00000110;
-   PORTD = 0b00001100;
-   PORTB = 0b00000001;
+    //SET DDR and PULL-UP
+    DDRC = 0b0111111;
+    DDRD = 0b11100000;
+    DDRB = 0b00000110;
+    PORTD = 0b00001100;
+    PORTB = 0b00000001;
 
    while(1){
     
-        wdt_reset();
-        pwm_fkt(pwm, sleep_mode_on);
-
-
-}
-
+    wdt_reset();
+    pwm_fkt(pwm, sleep_mode_on);
+    sleep(sleep_mode_on);
    
+    }
 }
 
 
@@ -106,5 +127,29 @@ ISR(TIMER0_COMPA_vect){
 
 pwm++;
 if(pwm>=200)pwm=0;
+
+}
+
+//SLEEP Taster interrupt
+ISR(INT0_vect){
+
+    if(prellS==0){
+
+        if(sleep_mode_on==false){
+
+        sleep_mode_on = true;
+        }
+
+        else{
+
+        sleep_mode_on = false;
+        }
+    }
+prellS = 90;
+
+}
+
+ISR(INT1_vect){
+
 
 }
